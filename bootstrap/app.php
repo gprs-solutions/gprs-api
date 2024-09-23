5cafd0cd99389b1f9bb335980d9e53bdbbf27151
Guilherme <guinovembro43@gmail.com>
2024-09-22 23:26:48 -0300

    handling cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(
         function (Middleware $middleware) {
-            // In case a general middleware is needed.
+            $middleware->append(HandleCors::class);
         }
     )
     ->withExceptions(
